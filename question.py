19b495493e745b75a6e5261eb4accd2e943a59bd
Ehsan Zumrut <ehsan@tamhub.com>
2019-04-29 22:41:47 +0300

    Add a class for question lists

---
+++ question.py
@@ -0,0 +1,30 @@
+class Question:
+    hi = ["hi", "hello", "salam", "hey"]
+
+    cuteness = [
+        "you are cute",
+        "you are beautiful",
+        "i like cats",
+        "i love cats",
+        "i like kittens",
+        "i love kitten",
+        "cute",
+        "the best kitty!",
+        "you are the best kitty!",
+    ]
+
+    provoking = [
+        "i prefer dogs",
+        "i prefer a dog",
+        "dogs are the best",
+        "you are annoying",
+        "attention seeker"
+    ]
+
+    adoption = [
+        "i want to live with a cat",
+        "i want to live with cats",
+        "i want to adopt a cat",
+        "how can I adopt a cat?",
+        "talk with me about adoption"
+    ]
