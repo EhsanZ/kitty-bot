e05db6c4b2d756ca23a26da7e3318fcae763cce9
Ehsan Zumrut <ehsan@tamhub.com>
2019-04-29 22:41:57 +0300

    Add a class for kitty responses

---
+++ kitty.py
@@ -0,0 +1,45 @@
+class Kitty:
+    hi = [
+        "hi",
+        "hello",
+        "meow meow!",
+        "hiiii ㅇㅅㅇ",
+        "චᆽච"
+    ]
+
+    adoption_agencies = [
+        "Check this website http://www.openpaws.org/",
+        "Check this forum https://www.expatriates.com/classifieds/riy/pets/ ( ⊙‿⊙)",
+        "Check this cool shekter http://riyadhshelter.org/en/ (๑♡3♡๑)"
+    ]
+
+    cuteness = [
+        "meow ʕ￫ᴥ￩ʔ",
+        "arigatooo (◕‿◕✿)",
+        "(◕ᴥ◕)",
+        "omg omg my fluffy cheeks!",
+        "you're so cute!",
+        "i love cat people",
+        "thanks thanks ><",
+        "(*^.^*)"
+    ]
+
+    anger = [
+        "WHAT (T⌓T)",
+        "You're making this kitty angry! (ಠ⌣ಠ)",
+        "Meow you 〴⋋_⋌〵"
+        "(⋋▂⋌)"
+    ]
+
+    please = [
+        "please talk with me!",
+        "Give me some attention T_T",
+        "talk!"
+    ]
+
+    idk = [
+        "I don't meow!",
+        "⁀⊙﹏☉⁀",
+        "I wasn't created to answer such easy questions (ಥ﹏ಥ)",
+        "Please make your question easier (ノAヽ)"
+    ]
